b64b8a70ed54625cd38926c1e70492de09934876
Paul Annesley <paul@annesley.cc>
2011-08-27 17:24:08 +1000

    Comment explaining fat_string_copy() in utils.c

--- utils.c
+++ utils.c
@@ -2,6 +2,12 @@
 
 #include "utils.h"
 
+/**
+ * Various strings in FAT aren't null-terminated.
+ * strncpy() on it's own doesn't guarantee null-terminated output.
+ * strlcpy() does, but it is non-standard and not present in glibc.
+ * output buffer must be at least length+1 bytes.
+ */
 void fat_string_copy(char * output, char * input, int length) {
   memset(output, '\0', length + 1);
   strncpy(output, input, length);
