80c5c5205a9f898397398835216ccbda1d24792c
Paul Annesley <paul@annesley.cc>
2011-08-28 13:00:51 +1000

    Tidied fat_open_filesystem(), removed unused variable.

--- filesystem.c
+++ filesystem.c
@@ -2,16 +2,13 @@
 #include "utils.h"
 
 void fat_open_filesystem(struct fat16_filesystem * fs, char * path) {
-
-  size_t bytes_read;
-
-  fs->fd = fopen(path, "r");
-  if (!fs->fd) fatal_error("unable to fopen filesystem");
+  if (!(fs->fd = fopen(path, "r"))) {
+    fatal_error("unable to fopen filesystem");
+  }
 
   if (!fread(&fs->boot_sector, sizeof(fs->boot_sector), 1, fs->fd)) {
     fatal_error("unable to read boot sector");
   }
-
 }
 
 void fat_close_filesystem(struct fat16_filesystem * fs) {
